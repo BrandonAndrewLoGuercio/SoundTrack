3ed59d7a12d3ced1cb23cdabc2a53ac765c23cb2
brandonandrewloguercio <brandonloguercio@yahoo.com>
2015-06-12 13:04:07 -0700

    Merge remote-tracking branch 'remotes/origin/followers' into Create-Post
    # Conflicts:
    #	.idea/SoundTrack.iml
    #	.idea/vcs.xml
    #	.idea/workspace.xml
    #	config/routes.rb
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612183536) do
+ActiveRecord::Schema.define(version: 20150612183949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "follows", force: :cascade do |t|
     t.integer  "following"
     t.integer  "follower"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
