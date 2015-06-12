88009acf89c959a8041aa2fd52ca2c91872c292c
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-06-12 16:39:44 -0700

    Updated user view and fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   has_many :follows
+  has_many :posts
   has_many :followers, through: :follows, class_name: 'User', source: :follower
   has_many :followings, through: :follows, class_name: 'User', source: :following
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :username
 end
