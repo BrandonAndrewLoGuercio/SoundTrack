29b1803c46b7534466db8e8ffcf7b2042a979fd5
Alexandra Bartholomew <alb@Alexandras-MacBook-Air.local>
2015-06-15 12:48:14 -0700

    Add additional tests for followings/followers relationships

--- spec/follower_spec.rb
+++ spec/follower_spec.rb
@@ -9,26 +9,40 @@ describe User do
 
   end
 
-  describe User do
-    it "should not have followers when first created" do
-      expect(@user1.followers).to eq([])
-      expect(@user2.followers).to eq([])
-    end
-
-
-    it "should be able to return followers" do
-      @user1.followers << @user2
-
-      expect(User.find(@user1.id).followers).to eq([@user2])
-    end
-
-    it "should be able to return all followers" do
-      @user1.followers << @user2
-      @user1.followers << @user3
-      @user1.followers << @user4
-
-      expect(User.find(@user1.id).followers).to match_array([@user2, @user3, @user4])
-    end
+  it "should not have followers when first created" do
+    expect(@user1.followers).to eq([])
+    expect(@user2.followers).to eq([])
   end
+
+
+  it "should be able to return followers" do
+    @user1.followers << @user2
+
+    expect(User.find(@user1.id).followers).to eq([@user2])
+  end
+
+  it "should be able to return all followers" do
+    @user1.followers << @user2
+    @user1.followers << @user3
+    @user1.followers << @user4
+
+    expect(User.find(@user1.id).followers).to match_array([@user2, @user3, @user4])
+  end
+
+
+  it "should be able to return followings" do
+    @user1.followings << @user2
+
+    expect(User.find(@user1.id).followings).to eq([@user2])
+  end
+
+  it "should be able to return all followers" do
+    @user1.followings << @user2
+    @user1.followings << @user3
+    @user1.followings << @user4
+
+    expect(User.find(@user1.id).followings).to match_array([@user2, @user3, @user4])
+  end
+
 
 end
