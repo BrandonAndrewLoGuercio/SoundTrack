0b7ef80adb044d343b19a99ebe694f0f1f4d4b12
brandonandrewloguercio <brandonloguercio@yahoo.com>
2015-06-16 11:02:23 -0700

    Added posting functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615175632) do
+ActiveRecord::Schema.define(version: 20150616174447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150615175632) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "message"
   end
 
   create_table "users", force: :cascade do |t|
