a1f4dd72b972edbc8ae72b4fde4dda147e9ab551
alb <alliebartholomew1@gmail.com>
2015-06-17 15:14:27 -0700

    Added followers and following counts to main feed/welcome view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616174442) do
+ActiveRecord::Schema.define(version: 20150616174447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150616174442) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "message"
   end
 
   create_table "relationships", force: :cascade do |t|
