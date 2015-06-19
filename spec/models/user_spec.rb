f94bfa1b81d77c314799e10524fce480b95651d7
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-06-19 13:04:55 -0700

    Working user tests with devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,59 +16,51 @@ RSpec.describe User, type: :model do
 
   it 'validates presence of username' do
     @user.username = nil
-    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Username can't be blank")
 
     @user.username = 'foo'
     expect(@user.username).to eq("foo")
   end
 
   it 'validates uniqueness of username' do
-    user1 = build(:user, username: @user.username)
-    expect {user1.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    @user1 = build(:user, username: @user.username, email:'foo@example.com')
+    expect {@user1.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Username has already been taken")
 
-    user2 = build(:user)
-    expect {user2.save!}.not_to raise_error
+    @user2 = build(:user)
+    expect {@user2.save!}.not_to raise_error
   end
 
   it 'validates presence of password' do
     @user.password = nil
-    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Password can't be blank")
 
     @user.password = "password1"
     expect(@user.password).to eq("password1")
   end
 
-  it 'validates length of password' do
+  it 'validates length of password to be at least 8 characters' do
     @user.password = 'abc'
-    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid)
-
+    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Password is too short (minimum is 8 characters)")
     @user.password = "password1"
     expect(@user.password).to eq("password1")
   end
 #
   it 'validates presence of email' do
     @user.email = nil
-    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    expect {@user.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email can't be blank")
 
     @user.email = "foo@foo.com"
     expect(@user.email).to eq("foo@foo.com")
   end
 
   it 'validates uniqueness of email' do
-    user1 = build(:user, email: @user.email)
-    expect {user1.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    @user1 = build(:user, email: @user.email)
+    expect {@user1.save!}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email has already been taken")
 
-    user2 = build(:user)
-    expect {user2.save!}.not_to raise_error
+    @user2 = build(:user)
+    expect {@user2.save!}.not_to raise_error
   end
-#
-#   it "authenticates with matching username and password" do
-#     expect(@user.authenticate(@user.username, @user.password)).to eq(user)
-#   end
-#
-#   it "does not authenticate with incorrect password" do
-#     expect(@user.authenticate(@user.username, "incorrect12")).to be_nil
-#   end
+
 end
 
 
