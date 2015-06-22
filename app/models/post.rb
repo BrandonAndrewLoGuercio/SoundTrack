59c8ec62c7ae5555283681a0135398bcabe9d471
treyking11 <treyking11@cox.net>
2015-06-22 14:36:22 -0700

    added post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  validates_format_of :url, with: /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
 end
--- spec/omniauth_facebook_user_sign_in_spec.rb
+++ spec/omniauth_facebook_user_sign_in_spec.rb
@@ -1,23 +1,24 @@
-require 'spec_helper'
+require 'rails_helper'
+RSpec.describe User do
+  it 'Signs in with Facebook' do
+    let! (:user) { create(:user, :with_facebook_account) }
 
-feature 'Sign in with Facebook' do
-  let! (:user) {create(:user, :with_facebook_account)}
+    background do
+      visit root_path
+      click_link "Sign in"
+      Omniauth.config.mock_auth[:facebook] = {
+          provider: 'facebook',
+          uid: user.facebook_account.uid,
+          credentials: {
+              token: 'facebook token'
+          }
+      }
+    end
 
-  background do
-    visit root_path
-    click_link "Sign in"
-    Omniauth.config.mock_auth[:facebook] = {
-        provider: 'facebook',
-        uid: user.facebook_account.uid,
-        credentials: {
-            token: 'facebook token'
-        }
-    }
-  end
+    it 'click sign in with facebook' do
+      click_link 'Sign In With Facebook'
 
-  scenario 'click sign in with facebook' do
-    click_link 'Sign In With Facebook'
-
-    page.should display_flash_message('Signed in successfully.')
+      page.should display_flash_message('Signed in successfully.')
+    end
   end
 end
