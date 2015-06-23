452f9efff03e5b779a2de888a1a354a7bf3b0ec8
treyking11 <treyking11@cox.net>
2015-06-23 10:46:27 -0700

    added url column to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  validates_format_of :url, with: /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
+  #validates_format_of :url, with: /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
+  #validates_format_of :url, :with => /\A(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617185422) do
+ActiveRecord::Schema.define(version: 20150623174008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150617185422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "message"
+    t.string   "url"
   end
 
   create_table "relationships", force: :cascade do |t|
