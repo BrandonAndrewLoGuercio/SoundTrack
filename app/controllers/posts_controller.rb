cdec0b12e8891aa76c8378e7771aca115032e34a
treyking11 <treyking11@cox.net>
2015-06-24 15:59:32 -0700

    resolved url validation for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params[:post].permit(:message)
+      params[:post].permit(:message, :url)
     end
 end
