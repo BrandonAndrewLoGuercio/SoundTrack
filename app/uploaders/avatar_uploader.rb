21b12c31b9382cb528684b97b97606c85cff4a6e
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-06-29 13:41:33 -0700

    Update application controller to accept avatar in parameters

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -19,8 +19,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    [version_name, "default.png"].compact.join('_')
+    ActionController::Base.helpers.asset_path([version_name, "default.png"].compact.join('_'))
+  #   [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
