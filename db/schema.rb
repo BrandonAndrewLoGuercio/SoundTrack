a831680cb1d47a8731c22afe52eb1e0709fbbf5b
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-07-02 12:23:04 -0700

    Made headline a user field and added ability to make headline for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630210041) do
+ActiveRecord::Schema.define(version: 20150702191252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20150630210041) do
     t.string   "provider"
     t.string   "uid"
     t.boolean  "admin",                  default: false
+    t.string   "headline"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
