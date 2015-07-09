1eee8bd2f9e371c37fc607b8d417071fbd182497
treyking11 <treyking11@cox.net>
2015-07-09 16:26:58 -0700

    punch heroku in the face

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   #
   # # Include default devise modules. Others available are:
   # # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable, :omniauthable,
+  devise :database_authenticatable, :registerable, :omniauthable,
   #        :recoverable, :rememberable, :trackable, :validatable
   #
   #
